990117869b3b7bf2c59884315cbda8f7563049b9
martinlanabengut <martinlanabengut@gmail.com>
2023-06-07 21:17:47 -0300

    2. LOAD
    store that in a Data Warehouse. We will use SQLite as our database engine to keep things simpler.
    
    src/load.py module: Load the dataframes into the sqlite database.

--- src/load.py
+++ src/load.py
@@ -10,9 +10,17 @@ def load(data_frames: Dict[str, DataFrame], database: Engine):
     Args:
         data_frames (Dict[str, DataFrame]): A dictionary with keys as the table names
         and values as the dataframes.
+        database (Engine): A SQLAlchemy Engine that represents the core interface to the database.
     """
     # TODO: Implement this function. For each dataframe in the dictionary, you must
     # use pandas.Dataframe.to_sql() to load the dataframe into the database as a
     # table.
     # For the table name use the `data_frames` dict keys.
-    raise NotImplementedError
+
+    try:
+        for table_name, df in data_frames.items():
+            df.to_sql(table_name, database, if_exists='replace')
+        print("Data loaded successfully into the database.")
+    except Exception as e:
+        print(f"An error occurred while loading data into the database: {e}")
+        raise
