717bf543a8bab5b3913f177beccf7a729d7b9a0a
martinlanabengut <martinlanabengut@gmail.com>
2023-06-21 20:01:22 -0300

    SQL QUERY

--- queries/top_10_revenue_categories.sql
+++ queries/top_10_revenue_categories.sql
@@ -5,3 +5,42 @@
 -- last one will be Revenue, with the total revenue of each catgory.
 -- HINT: All orders should have a delivered status and the Category and actual 
 -- delivery date should be not null.
+
+SELECT
+    category_translation.product_category_name_english AS Category,
+    COUNT(DISTINCT orders.order_id) AS Num_orders,
+    SUM(order_payments.payment_value) AS Revenue
+FROM
+    olist_orders orders
+        INNER JOIN
+    olist_order_items order_items
+    ON
+            orders.order_id = order_items.order_id
+        INNER JOIN
+    olist_products products
+    ON
+            order_items.product_id = products.product_id
+        INNER JOIN
+    product_category_name_translation category_translation
+    ON
+            products.product_category_name = category_translation.product_category_name
+        INNER JOIN
+    olist_order_payments order_payments
+    ON
+            orders.order_id = order_payments.order_id
+WHERE
+        orders.order_status = 'delivered'
+  AND orders.order_delivered_customer_date IS NOT NULL
+  AND products.product_category_name IS NOT NULL
+GROUP BY
+    category_translation.product_category_name_english
+ORDER BY
+    Revenue DESC
+LIMIT 10;
+
+
+
+
+
+
+
